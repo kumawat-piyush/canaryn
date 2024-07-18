999e800758e2c7ef9f3284169f6d27deb734a4cd
Tan Nhu <tnhu@users.noreply.github.com>
2024-07-17 22:22:25 -0700

    Attempt to fix commonjs--resolver issue when building in CI env

--- packages/canary/vite.config.ts
+++ packages/canary/vite.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@/lib': path.resolve(__dirname, 'src', 'lib'),
+      '@/components': path.resolve(__dirname, 'src', 'components')
     }
   },
   build: {
