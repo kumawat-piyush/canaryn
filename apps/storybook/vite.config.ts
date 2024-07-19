7f2633563ea1b6a59107984158683437d0d2d784
Tan Nhu <tnhu@users.noreply.github.com>
2024-07-18 18:46:39 -0700

    Use file:

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+// import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,17 +8,17 @@ export default defineConfig({
   build: {
     sourcemap: true,
     copyPublicDir: false
-  },
-  resolve: {
-    alias: [
-      {
-        find: /@harnessio\/svg-icon/,
-        replacement: path.resolve('..', '..', 'packages', 'svg-icon')
-      },
-      {
-        find: /@harnessio\/svg-icon-react/,
-        replacement: path.resolve('..', '..', 'packages', 'svg-icon-react')
-      }
-    ]
   }
+  // resolve: {
+  //   alias: [
+  //     {
+  //       find: /@harnessio\/svg-icon/,
+  //       replacement: path.resolve('..', '..', 'packages', 'svg-icon')
+  //     },
+  //     {
+  //       find: /@harnessio\/svg-icon-react/,
+  //       replacement: path.resolve('..', '..', 'packages', 'svg-icon-react')
+  //     }
+  //   ]
+  // }
 })
