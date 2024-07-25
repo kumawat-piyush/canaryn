a39866270611732a441b24b82ca4a8ad1d759832
Abhinav Rastogi <abhinav.rastogi@harness.io>
2024-07-24 17:38:25 -0700

    feat: studio integration

--- apps/storybook/src/stories/screens/CreatePipeline.stories.tsx
+++ apps/storybook/src/stories/screens/CreatePipeline.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../../components/layout/container'
 import { GitnessNavbar } from '../components/NavBar.stories'
 import { GitnessTopBar } from '../components/TopBar.stories'
@@ -7,6 +7,10 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { Button, Form, FormControl, FormField, FormItem, FormMessage, Input } from '@harnessio/canary'
 import { MagicWandIcon, Pencil1Icon } from '@radix-ui/react-icons'
+import { PipelineStudio } from '../../../../../packages/unified-pipeline/src/components/PipelineStudio/PipelineStudio.tsx'
+import { getNodesFromPipelineYaml } from '../../../../../packages/unified-pipeline/src/utils/PipelineYamlUtils.ts'
+// import pipe_yaml from './demo.yaml'
+import {parse} from 'yaml'
 
 export default {
   title: 'Screens/Pipeline',
@@ -20,24 +24,21 @@ const formSchema = z.object({
 })
 
 export function CreatePipeline() {
+  const [state, setState] = useState()
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      prompt: 'Create a pipeline to build and test a go binary'
+      prompt: 'build me a python build pipeline'
     }
   })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    fetch('https://localhost:3000/api/v1/aiagent/generatePipeline', {
+    fetch('http://localhost:3000/api/v1/aiagent/generatePipeline', {
       method: 'POST',
       body: JSON.stringify({
         prompt: values.prompt,
-        repo: {
-          accountIdentifier,
-          orgIdentifier,
-          projectIdentifier,
-          repoIdentifier
-        }
+        repo_ref: 'test/uuid'
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -45,10 +46,10 @@ export function CreatePipeline() {
     })
       .then(res => res.json())
       .then(data => {
-        console.log(data)
+        setState(parse(data.YAML))
       })
   }
-
+console.log({state})
   return (
     <Container.Root>
       <Container.Sidebar>
@@ -58,7 +59,8 @@ export function CreatePipeline() {
         <Container.Topbar>
           <GitnessTopBar />
         </Container.Topbar>
-        <div className="flex flex-col gap-7 w-[770px] mx-auto my-7">
+        <div className="flex flex-col gap-7 my-7">
+          <div className="flex flex-col gap-4 w-[770px] mx-auto">
           <h1 className="text-lg">Create Pipeline</h1>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -74,17 +76,23 @@ export function CreatePipeline() {
                   </FormItem>
                 )}
               />
-              <div className='flex gap-4'>
-              <Button type="submit" variant={'outline'} className='rounded-full'>
-                <MagicWandIcon className='mr-2' /> Create with AI
-              </Button>
-              <Button variant={'outline'} className='rounded-full'>
-                <Pencil1Icon className='mr-2' /> Start from scratch
-              </Button>
+              <div className="flex gap-4">
+                <Button type="submit" variant={'outline'} className="rounded-full">
+                  <MagicWandIcon className="mr-2" /> Create with AI
+                </Button>
+                <Button variant={'outline'} className="rounded-full">
+                  <Pencil1Icon className="mr-2" /> Start from scratch
+                </Button>
               </div>
-
             </form>
           </Form>
+          </div>
+            {state && <PipelineStudio
+              nodes={getNodesFromPipelineYaml(state)}
+              onAddNode={() => {}}
+              onDeleteNode={() => {}}
+              onSelectNode={() => {}}
+            />}
         </div>
       </Container.Main>
     </Container.Root>
