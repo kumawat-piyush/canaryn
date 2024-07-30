f6a6bea4e4efbc9e932c2ac266643eeb04e53cd6
Srdjan Arsic <srdjan.arsic@harness.io>
2024-07-30 17:37:38 +0200

    yaml editor update v0.4.0

--- packages/yaml-editor/src/hooks/useSchema.tsx
+++ packages/yaml-editor/src/hooks/useSchema.tsx
@@ -1,27 +1,27 @@
-import { RefObject, useEffect } from "react";
-import * as monaco from "monaco-editor";
+import { RefObject, useEffect } from 'react'
+import * as monaco from 'monaco-editor'
 
-import { addSchema, schemaIdToUrl } from "../utils/schema-utils";
+import { addSchema, schemaIdToUrl } from '../utils/schema-utils'
 
 export type UseSchema = (arg: {
-  monacoRef: RefObject<typeof monaco | undefined>;
-  schemaConfig?: { schema: any; uri: string };
-  instanceId: string;
-}) => void;
+  monacoRef: RefObject<typeof monaco | undefined>
+  schemaConfig?: { schema: any; uri: string }
+  instanceId: string
+}) => void
 
 export const useSchema: UseSchema = (props): void => {
-  const { monacoRef, schemaConfig, instanceId } = props;
+  const { monacoRef, schemaConfig, instanceId } = props
 
   useEffect(() => {
-    if (!monacoRef.current || !schemaConfig?.schema) return;
+    if (!monacoRef.current || !schemaConfig?.schema) return
 
     addSchema(
       {
         // If YAML file is opened matching this glob
         fileMatch: [schemaIdToUrl(instanceId.toString())],
-        ...schemaConfig,
+        ...schemaConfig
       },
       monacoRef.current
-    );
-  }, [monacoRef, schemaConfig, instanceId]);
-};
+    )
+  }, [monacoRef, schemaConfig, instanceId])
+}
--- packages/yaml-editor/src/utils/schema-utils.ts
+++ packages/yaml-editor/src/utils/schema-utils.ts
@@ -1,24 +1,28 @@
-import { Uri } from "monaco-editor";
-import { configureMonacoYaml } from "monaco-yaml";
+import { Uri } from 'monaco-editor'
+import { configureMonacoYaml, MonacoYaml } from 'monaco-yaml'
+
+// TODO: temporary holds handle (used to dispose)
+let prevSchema: MonacoYaml
 
 export function addSchema(schemaConfig: any, monaco: any) {
-  applySchema(monaco, [schemaConfig]);
+  if (prevSchema) prevSchema.dispose()
+  prevSchema = applySchema(monaco, [schemaConfig])
 }
 
 function applySchema(monaco: any, schemas: any) {
-  configureMonacoYaml(monaco as any, {
+  return configureMonacoYaml(monaco as any, {
     hover: true,
     completion: true,
     enableSchemaRequest: false,
     validate: true,
-    schemas: schemas,
-  });
+    schemas: schemas
+  })
 }
 
 export const schemaIdToUrl = (id: string): string => {
-  return Uri.parse(`file://${id}`).toString();
-};
+  return Uri.parse(`file://${id}`).toString()
+}
 
 export const schemaIdToUri = (id: string): Uri => {
-  return Uri.parse(schemaIdToUrl(id));
-};
+  return Uri.parse(schemaIdToUrl(id))
+}
