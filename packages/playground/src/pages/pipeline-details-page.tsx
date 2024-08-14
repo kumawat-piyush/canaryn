09b599019185715b7c75bece7f3a27d8bde89d63
Abhinav Rastogi <abhinav.rastogi@harness.io>
2024-08-14 12:18:41 -0700

    cleanup pipeline outlets

--- packages/playground/src/pages/pipeline-details-page.tsx
+++ packages/playground/src/pages/pipeline-details-page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 import PipelineDetails from '../components/pipeline-details'
 
 export default function PipelineDetailsPage() {
   return (
     <>
-      <Outlet />
       <PipelineDetails />
     </>
   )
--- packages/playground/src/pages/pipeline-list-page.tsx
+++ packages/playground/src/pages/pipeline-list-page.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 import PipelineList from '../components/pipeline-list'
 
 export default function PipelineListPage() {
   return (
     <>
-      <Outlet />
       <PipelineList />
     </>
   )
