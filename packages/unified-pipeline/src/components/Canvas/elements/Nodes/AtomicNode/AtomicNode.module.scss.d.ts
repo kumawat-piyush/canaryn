c924e37aca9f2785e44f1b877d548c80a458ad11
Vardan Bansal <vardan.bansal@harness.io>
2024-08-19 17:14:22 -0700

    Migration to tailwind for Atomic node - done

--- packages/unified-pipeline/src/components/Canvas/elements/Nodes/AtomicNode/AtomicNode.module.scss.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const contentLayer: string
-export declare const gradientBorderGlow: string
-export declare const plus: string
-export declare const skeletonIcon: string
-export declare const skeletonLine: string
-export declare const skeletonLine1: string
-export declare const skeletonLine2: string
-export declare const skeletonSquare: string
--- packages/unified-pipeline/src/components/Canvas/elements/Nodes/AtomicNode/AtomicNode.tsx
+++ packages/unified-pipeline/src/components/Canvas/elements/Nodes/AtomicNode/AtomicNode.tsx
@@ -3,7 +3,7 @@ import cx from 'classnames'
 import type { NodeProps } from 'reactflow'
 import { Handle, Position, useReactFlow, Node } from 'reactflow'
 import { Plus, Computer } from '@harnessio/icons-noir'
-import { Text } from '@harnessio/canary'
+import { Skeleton, Text } from '@harnessio/canary'
 import {
   PositionType,
   type DefaultNodeProps,
@@ -18,8 +18,6 @@ import { Status } from '../../../../../utils/Constants'
 // import cardBg from '../../../../../assets/images/card-glow.svg'
 import { DEFAULT_NODE_LOCATION } from '../../../../../components/Canvas/utils/LROrientation/Constants'
 
-import css from './AtomicNode.module.scss'
-
 export interface AtomicNodeProps extends DefaultNodeProps, ExpandNodeProps, DeleteNodeProps {
   /**
    * @TODO add optional custom renderer props later
@@ -32,7 +30,7 @@ export default function AtomicNode({ isConnectable, data, id, xPos, yPos, zIndex
   const { enableDiagnostics } = useCanvasStore()
   const [width] = useState<number>(STEP_NODE_WIDTH)
   const [height] = useState<number>(STEP_NODE_HEIGHT)
-  const [status] = useState(Status.DONE)
+  const [status] = useState(Status.QUEUED)
   const [showPlus, setShowPlus] = useState<boolean>(false)
 
   const handleNodeDelete = useCallback(
@@ -78,22 +76,20 @@ export default function AtomicNode({ isConnectable, data, id, xPos, yPos, zIndex
       <Handle type="target" position={Position.Left} isConnectable={isConnectable} />
       {status === Status.QUEUED ? (
         <div>
-          <div
-            className={cx(
-              'border',
-              /* gradient */
-              'p-px rounded-md',
-              css.gradientBorderGlow
-            )}>
+          <div className="border p-px rounded-md gradient-border-glow">
             <div
               style={{
                 width,
                 height
               }}
-              className={css.contentLayer}>
-              <div className={cx(css.skeletonSquare, css.skeletonIcon)} />
-              <div className={cx(css.skeletonLine, css.skeletonLine1)} />
-              <div className={cx(css.skeletonLine, css.skeletonLine2)} />
+              className="content-layer p-2.5 rounded-md">
+              <div className="flex flex-col gap-3">
+                <Skeleton className="w-6 h-6" />
+                <div className="flex flex-col gap-1">
+                  <Skeleton className="w-[115px] h-[5px]" />
+                  <Skeleton className="w-[79px] h-[5px]" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -110,7 +106,7 @@ export default function AtomicNode({ isConnectable, data, id, xPos, yPos, zIndex
               width,
               height
             }}
-            className={'content-layer p-2.5 rounded-md'}>
+            className="content-layer p-2.5 rounded-md">
             {icon}
             <Text className="text-[11px] text-[rgba(255,255,255,1)] font-normal leading-4">{name}</Text>
             {enableDiagnostics?.Node && (
@@ -121,9 +117,13 @@ export default function AtomicNode({ isConnectable, data, id, xPos, yPos, zIndex
       )}
       <Handle type="source" position={Position.Right} isConnectable={isConnectable}>
         <Plus
-          className={cx('hover:cursor-pointer', css.plus, {
-            ['transition-opacity duration-200 ease-in-out opacity-100']: showPlus
-          })}
+          className={cx(
+            'hover:cursor-pointer',
+            'rounded-full w-5 h-5 opacity-0 border border-[rgba(48,48,54,0.6)] bg-[rgba(29,29,32,1)] text-[rgba(228,228,231,1)] transform -translate-x-2 -translate-y-2',
+            {
+              ['transition-opacity duration-200 ease-in-out opacity-100']: showPlus
+            }
+          )}
         />
       </Handle>
     </div>
