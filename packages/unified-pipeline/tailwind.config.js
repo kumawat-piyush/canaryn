a5b07a0474364be879459c766327f85a22cb7919
Vardan Bansal <vardan.bansal@harness.io>
2024-08-20 15:38:54 -0700

    Adding sample color in tailwind config for hardcoded color removal

--- packages/unified-pipeline/tailwind.config.js
+++ packages/unified-pipeline/tailwind.config.js
@@ -5,5 +5,14 @@ module.exports = {
     './src/**/*.{ts,tsx}',
     /* Required to watch on changes from styles.css, including any new utilities added with @layer */
     './src/styles.css'
-  ]
+  ],
+  theme: {
+    extend: {
+      colors: {
+        studio: {
+          1: 'var(--studio-1)'
+        }
+      }
+    }
+  }
 }
