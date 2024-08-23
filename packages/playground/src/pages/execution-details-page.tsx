3b21a9a605742fe345545429d3ddb2d892eb7e9f
Abhinav Rastogi <abhinav.rastogi@harness.io>
2024-08-22 23:00:12 -0700

    fix execution details page layout

--- packages/playground/src/pages/execution-details-page.tsx
+++ packages/playground/src/pages/execution-details-page.tsx
@@ -16,7 +16,7 @@ export default function ExecutionDetailsPage() {
   return (
     <Tabs variant="navigation" defaultValue={ExecutionTab.LOG}>
       {/* tab height */}
-      <TabsList className="h-11 flex gap-6 items-center justify-start border-solid border-b border-t">
+      <TabsList>
         <TabsTrigger value={ExecutionTab.SUMMARY}>Summary</TabsTrigger>
         {/* Need to manually adjust height to make bottom border align with parent */}
         <TabsTrigger className="h-full" value={ExecutionTab.LOG}>
