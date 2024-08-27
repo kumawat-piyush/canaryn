71d385ff22678d3c6ba8ad2e00c97cc9f7c4bac5
jessieyu777 <c_peipeijessie@harness.io>
2024-08-27 12:58:24 -0700

    add simple data

---
+++ packages/playground/src/components/execution/mocks/mockInput.ts
@@ -0,0 +1,15 @@
+//simple data for rendering
+export const data = [
+  {
+    name: 'Input Value',
+    value: 'canaryDeployment'
+  },
+  {
+    name: 'Type',
+    value: 'K8sCanaryDeploy'
+  },
+  {
+    name: 'timeout',
+    value: '10m'
+  }
+]
