51d0fa8a4c720361272cac00b79cd95a1c80e990
Vardan Bansal <vardan.bansal@harness.io>
2024-08-26 22:11:26 -0700

    adding temp request headers

--- apps/gitness/src/App.tsx
+++ apps/gitness/src/App.tsx
@@ -3,8 +3,14 @@ import { Button } from '@harnessio/canary'
 import { useListPipelines } from './services/code'
 
 export default function App() {
-  const { data } = useListPipelines({ repo_ref: 'workspace/repo/+' })
-  console.log(data)
+  const retHeaders: RequestInit['headers'] = {
+    'content-type': 'application/json'
+  }
+  const token = localStorage.getItem('token')
+  if (token && token.length > 0) {
+    retHeaders.Authorization = `Bearer ${token}`
+  }
+  useListPipelines({ repo_ref: 'workspace/repo/+', requestOptions: { headers: retHeaders } })
   return (
     <div className="flex flex-col">
       <Button>Test</Button>
