fb0296d3d8a710d8347f75fbb05de53803d2fefa
joetaylorland <accounts+github@joetaylor.net>
2024-08-27 12:33:01 +0100

    clean up

--- packages/playground/src/components/commits-list.tsx
+++ packages/playground/src/components/commits-list.tsx
@@ -3,15 +3,14 @@ import { Text } from '@harnessio/canary'
 
 export default function CommitsList() {
   return (
-    <div className="grid grid-cols-[220px_1fr] h-[calc(100vh-102px)] overflow-hidden">
-      {/* Left Column */}
-      <div className="h-full overflow-y-auto border-r px-8 py-4 bg-primary/[0.03]">
+    //  Needs absolute height here unfortunately, h-full does not work
+    <div className="grid grid-cols-[220px_1fr] h-[calc(100vh-102px)]">
+      <div className="h-full overflow-y-auto border-r px-8 py-4">
         <Text size={1}>Left child panel</Text>
         <div className="h-[2000px]" />
         <Text size={2}>End of left</Text>
       </div>
-      {/* Right Column */}
-      <div className="h-full overflow-y-auto px-8 py-4 bg-primary/[0.04]">
+      <div className="h-full overflow-y-auto px-8 py-4">
         <Text size={1}>Right child panel</Text>
         <div className="h-[2000px]" />
         <Text size={2}>End of right</Text>
