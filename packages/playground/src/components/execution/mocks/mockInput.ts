f63a134229ed69f7a9f881c06d08d478050fc2ef
jessieyu777 <c_peipeijessie@harness.io>
2024-08-27 17:11:13 -0700

    add longer data to test the scrollarea

--- packages/playground/src/components/execution/mocks/mockInput.ts
+++ packages/playground/src/components/execution/mocks/mockInput.ts
@@ -1,15 +1,87 @@
 //simple data for rendering
 export const inputData = [
   {
-    name: 'Input Value',
+    name: 'Input Name',
+    value: 'Input Value'
+  },
+  {
+    name: 'identifier',
     value: 'canaryDeployment'
   },
   {
-    name: 'Type',
-    value: 'K8sCanaryDeploy'
+    name: 'name',
+    value: 'canaryDeployment'
   },
   {
     name: 'timeout',
     value: '10m'
+  },
+  {
+    name: 'type',
+    value: 'K8sCanaryDeploy'
+  },
+  {
+    name: 'type',
+    value: 'percentage'
+  },
+  {
+    name: 'identifier',
+    value: 'canaryDeployment'
+  },
+  {
+    name: 'name',
+    value: 'canaryDeployment'
+  },
+  {
+    name: 'timeout',
+    value: '10m'
+  },
+  {
+    name: 'type',
+    value: 'K8sCanaryDeploy'
+  },
+  {
+    name: 'type',
+    value: 'percentage'
+  },
+  {
+    name: 'identifier',
+    value: 'canaryDeployment'
+  },
+  {
+    name: 'name',
+    value: 'canaryDeployment'
+  },
+  {
+    name: 'timeout',
+    value: '10m'
+  },
+  {
+    name: 'type',
+    value: 'K8sCanaryDeploy'
+  },
+  {
+    name: 'type',
+    value: 'percentage'
+  },
+  {
+    name: 'identifier',
+    value: 'canaryDeployment'
+  },
+  {
+    name: 'name',
+    value: 'canaryDeployment'
+  },
+  {
+    name: 'timeout',
+    value: '10m'
+  },
+  {
+    name: 'type',
+    value: 'K8sCanaryDeploy'
+  },
+  {
+    name: 'type',
+    value: 'percentage'
   }
 ]
