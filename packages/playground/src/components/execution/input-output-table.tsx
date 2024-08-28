f2c15880d9bd0116bc633a8a1ea70b2b38a12c1f
jessieyu777 <c_peipeijessie@harness.io>
2024-08-27 17:02:29 -0700

    fix the issue of width changing which is not get the parent style: w-2/3 & table cell style

--- packages/playground/src/components/execution/input-output-table.tsx
+++ packages/playground/src/components/execution/input-output-table.tsx
@@ -1,32 +1,40 @@
 import React from 'react'
 import { ScrollArea } from '@harnessio/canary'
-import { Table, TableBody, TableCell, TableHeader, TableHead, TableRow } from '@harnessio/canary'
-
+import { Table, TableBody, TableCell, TableHeader, TableHead, TableRow, Text } from '@harnessio/canary'
 interface InputTableProps {
   inputData: { name: string; value: string }[]
+  className?: string
 }
 
-export const InputOutputTable: React.FC<InputTableProps> = ({ inputData }) => {
+export const InputOutputTable: React.FC<InputTableProps> = ({ inputData, className }) => {
   const titleKey = inputData[0].name
   const titleValue = inputData[0].value
 
   const dataContent = inputData.slice(1)
 
   return (
-    <ScrollArea className="overflow-x-auto">
-      <Table>
+    <ScrollArea className="overflow-x-auto pt-4">
+      <Table className={className}>
         <TableHeader>
           <TableRow>
-            <TableHead>{titleKey}</TableHead>
-            <TableHead>{titleValue}</TableHead>
+            <TableHead>
+              <Text size={3} weight="bold" style={{ color: '#fff' }}>
+                {titleKey}
+              </Text>
+            </TableHead>
+            <TableHead>
+              <Text size={3} weight="bold" style={{ color: '#fff' }}>
+                {titleValue}
+              </Text>
+            </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {dataContent.map((item: { name: string; value: string }) => {
             return (
               <TableRow key={item.name}>
-                <TableCell>{item.name}</TableCell>
-                <TableCell>{item.value}</TableCell>
+                <TableCell className="pt-2.5 pl-4">{item.name}</TableCell>
+                <TableCell className="pt-2.5 pl-4">{item.value}</TableCell>
               </TableRow>
             )
           })}
