5e60aafa4341439ab7fb2e3d556791392f35a6c1
Vardan Bansal <vardan.bansal@harness.io>
2024-08-30 08:48:19 -0700

    fix typecheck

--- apps/gitness/src/pages/PipelineList.tsx
+++ apps/gitness/src/pages/PipelineList.tsx
@@ -1,25 +1,22 @@
 import { useListPipelinesQuery, TypesPipeline } from '@harnessio/code-service-client'
 
 export default function Pipelines() {
-  const { data } = useListPipelinesQuery(
+  const { data: pipelines } = useListPipelinesQuery(
     {
       repo_ref: 'workspace/repo/+',
       queryParams: { page: 0, limit: 10, query: '', latest: true }
+    },
+    /* To enable mock data */
+    {
+      placeholderData: [{ identifier: 'pipeline1' }, { identifier: 'pipeline2' }],
+      enabled: true
     }
-    // /* To enable mock data */
-    // {
-    //   placeholderData: { content: [{ id: 'pipeline1' }, { id: 'pipeline2' }] },
-    //   enabled: true
-    // }
   )
-  const pipelines: TypesPipeline[] = data?.content || []
 
   return (
     <div className="flex flex-col justify-center">
       <h1>Pipelines</h1>
-      {pipelines.map(pipeline => (
-        <div key={pipeline.id}>{pipeline.id}</div>
-      ))}
+      {pipelines?.map((pipeline: TypesPipeline) => <div key={pipeline.id}>{pipeline.id}</div>)}
     </div>
   )
 }
