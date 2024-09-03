e6748cf346f0cd41d8a404267688277bb70f39d3
joetaylorland <accounts+github@joetaylor.net>
2024-09-03 15:36:24 +0100

    add splitbutton variant and theme according to error|warning|success

--- packages/canary/src/components/button.tsx
+++ packages/canary/src/components/button.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
-
 import { cn } from '@/lib/utils'
 import { CanaryOutletFactory, CanaryOutletName } from '@/lib/CanaryOutletFactory'
 
@@ -16,7 +15,8 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         tertiary: 'bg-tertiary text-secondary-foreground shadow-sm hover:bg-tertiary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline'
+        link: 'text-primary underline-offset-4 hover:underline',
+        split: 'border flex items-center gap-1.5 p-0'
       },
       size: {
         default: 'h-9 px-4 py-2',
@@ -24,11 +24,18 @@ const buttonVariants = cva(
         xs: 'h-auto py-0.5 rounded-md px-1.5 text-xs font-normal',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
-        sm_icon: 'h-7 w-7'
+        sm_icon: 'h-7 w-7',
+        xs_split: 'h-auto p-0 rounded-md text-xs font-medium'
       },
       borderRadius: {
         default: '',
         full: 'rounded-full'
+      },
+      theme: {
+        default: '',
+        error: 'text-error border-[hsla(var(--error),0.3)] bg-[hsla(var(--error),0.1)]',
+        warning: 'text-warning border-[hsla(var(--warning),0.3)] bg-[hsla(var(--warning),0.1)]',
+        success: 'text-success border-[hsla(var(--success),0.3)] bg-[hsla(var(--success),0.1)]'
       },
       padding: {
         default: '',
@@ -38,6 +45,7 @@ const buttonVariants = cva(
     defaultVariants: {
       variant: 'default',
       size: 'default',
+      theme: 'default',
       padding: 'default',
       borderRadius: 'default'
     }
@@ -50,6 +58,7 @@ export interface ButtonProps
   asChild?: boolean
   loading?: boolean
   spinner?: React.ReactNode
+  dropdown?: React.ReactNode
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
@@ -59,17 +68,20 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       variant,
       size,
       padding,
+      theme,
       borderRadius,
       asChild = false,
       loading,
       disabled,
       spinner,
+      dropdown,
       children,
       ...props
     },
     ref
   ) => {
     const Comp = asChild ? Slot : 'button'
+
     const _children = loading ? (
       <>
         {(loading && spinner) ||
@@ -77,6 +89,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             className,
             variant,
             size,
+            theme,
             padding,
             borderRadius,
             asChild,
@@ -90,13 +103,22 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ) : (
       children
     )
+
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, padding, borderRadius, className }))}
+        className={cn(buttonVariants({ variant, size, theme, padding, borderRadius, className }))}
         ref={ref}
         disabled={disabled || loading}
-        {...props}
-        children={_children}></Comp>
+        {...props}>
+        {variant === 'split' && dropdown ? (
+          <>
+            <div className="flex pl-2.5 pr-1 py-0.5 items-center">{_children}</div>
+            {dropdown && dropdown}
+          </>
+        ) : (
+          _children
+        )}
+      </Comp>
     )
   }
 )
