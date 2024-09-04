40cbe8105cc6d47d2dc7da0c5f639e34bff63d09
joetaylorland <accounts+github@joetaylor.net>
2024-09-04 14:15:34 +0100

    added two empty views for signin and signup

--- packages/playground/src/components/signin.tsx
+++ packages/playground/src/components/signin.tsx
@@ -1,5 +1,14 @@
+import { SpotlightsBG, Text } from '@harnessio/canary'
 import React from 'react'
 
 export default function Signin() {
-  return <div>Sign in</div>
+  return (
+    <SpotlightsBG.Root>
+      <SpotlightsBG.Content>
+        <Text size={6} weight="medium" align="center" color="primary">
+          Sign in
+        </Text>
+      </SpotlightsBG.Content>
+    </SpotlightsBG.Root>
+  )
 }
