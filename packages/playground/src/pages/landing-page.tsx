4088b00b6f74121bb2f3ae7fb516f66accc67226
joetaylorland <accounts+github@joetaylor.net>
2024-09-04 13:58:00 +0100

    add two home page states with linkages

--- packages/playground/src/pages/landing-page.tsx
+++ packages/playground/src/pages/landing-page.tsx
@@ -3,16 +3,30 @@ import Home from '../components/home'
 import PlaygroundLandingSettings from '../settings/landing-settings'
 import Signin from '../components/signin'
 import Signup from '../components/signup'
+import { useNavigate } from 'react-router-dom'
 
 export default function LandingPage() {
-  const [loadState, setLoadState] = useState('home-unauth')
+  const [loadState, setLoadState] = useState('home-auth')
+  const navigate = useNavigate()
+
+  const handleSelectProject = () => {
+    navigate('/repos')
+  }
+
+  const handleSignUp = () => {
+    setLoadState('sign-up')
+  }
+
+  const handleSignIn = () => {
+    setLoadState('sign-in')
+  }
 
   const renderContent = () => {
     switch (loadState) {
       case 'home-auth':
-        return <Home isAuthed />
+        return <Home isAuthed onSelectProject={handleSelectProject} />
       case 'home-unauth':
-        return <Home isAuthed={false} />
+        return <Home isAuthed={false} handleSignUp={handleSignUp} handleSignIn={handleSignIn} />
       case 'sign-in':
         return <Signin />
       case 'sign-up':
