b95f7c45373be5cce793fc4b263aa6c7a8942c1d
joetaylorland <accounts+github@joetaylor.net>
2024-09-04 12:02:50 +0100

    first pass at making home page accept playground states: landing (auth) | landing (unauth) | signup | signin

--- packages/playground/src/pages/home-page.tsx
+++ packages/playground/src/pages/home-page.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from '../components/home'
+import SkeletonList from '../components/loaders/skeleton-list'
+import NoData from '../components/no-data'
+import PlaygroundHomeSettings from '../settings/home-settings'
 
-export default function HomePageView() {
-  return <Home />
+export default function HomeListPage() {
+  const [loadState, setLoadState] = useState('data-loaded')
+
+  const renderContent = () => {
+    switch (loadState) {
+      case 'data-loaded':
+        return <Home />
+      case 'loading':
+        return <SkeletonList />
+
+      default:
+        return null
+    }
+  }
+
+  if (loadState == 'no-data') {
+    return (
+      <>
+        <NoData iconName="no-data-folder" title="No Home yet" description={['There are no Home yet.']} />
+        <PlaygroundHomeSettings loadState={loadState} setLoadState={setLoadState} />
+      </>
+    )
+  }
+  return (
+    <>
+      {renderContent()}
+      <PlaygroundHomeSettings loadState={loadState} setLoadState={setLoadState} />
+    </>
+  )
 }
