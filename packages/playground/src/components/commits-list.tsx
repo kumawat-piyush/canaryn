036205efb92e180dd912f9a8e0ba74c6e0014880
joetaylorland <accounts+github@joetaylor.net>
2024-09-08 12:49:41 +0100

    merge latest

--- packages/playground/src/components/commits-list.tsx
+++ packages/playground/src/components/commits-list.tsx
@@ -1,4 +1,4 @@
-import { Badge, cn, Icon, StackedList } from '@harnessio/canary'
+import { Icon, NodeGroup, StackedList, Text } from '@harnessio/canary'
 import React from 'react'
 
 interface Repo {
@@ -17,64 +17,106 @@ interface PageProps {
   LinkComponent: React.ComponentType<{ to: string; children: React.ReactNode }>
 }
 
-const Stats = ({ stars, forks, pulls }: { stars: number; forks: number; pulls: number }) => (
-  <div className="flex gap-3 justify-end items-center select-none font-medium">
-    <span className="flex gap-1.5 items-center">
-      <Icon width={16} name="star" className="text-tertiary-background" />
-      <span className="text-primary text-xs font-normal">{stars || 0}</span>
-    </span>
-    <span className="flex gap-1.5 items-center">
-      <Icon size={16} name="pull" className="text-tertiary-background" />
-      <span className="text-primary text-xs font-normal">{forks || 0}</span>
-    </span>
-    <span className="flex gap-1.5 items-center">
-      <Icon size={16} name="pull" className="text-tertiary-background" />
-      <span className="text-primary text-xs font-normal">{pulls || 0}</span>
-    </span>
-  </div>
-)
-
-const Title = ({ title, isPrivate }: { title: string; isPrivate: boolean }) => (
-  <>
-    {title}
-    <Badge
-      size="sm"
-      disableHover
-      className={cn('ml-3 rounded-full border border-[hsla(var(--success),0.4)] text-success bg-transparent', {
-        'border border-muted bg-background text-tertiary-background': isPrivate
-      })}>
-      {isPrivate ? 'Private' : 'Public'}
-    </Badge>
-  </>
-)
+const Title = ({ title }: { title: string }) => <>{title}</>
 
 export default function RepoList({ repos, LinkComponent }: PageProps) {
   return (
     <>
       {repos && repos.length > 0 && (
-        <StackedList.Root>
-          {repos.map((repo, repo_idx) => (
-            <LinkComponent to={repo.name}>
-              <StackedList.Item key={repo.name} isLast={repos.length - 1 === repo_idx}>
-                <StackedList.Field
-                  description={repo.description}
-                  title={<Title title={repo.name} isPrivate={repo.private} />}
-                />
-                <StackedList.Field
-                  title={
-                    <>
-                      Updated <em>{repo.timestamp}</em>
-                    </>
-                  }
-                  description={<Stats stars={repo.stars} forks={repo.forks} pulls={repo.pulls} />}
-                  right
-                  label
-                  secondary
-                />
-              </StackedList.Item>
-            </LinkComponent>
-          ))}
-        </StackedList.Root>
+        <NodeGroup.Root>
+          <NodeGroup.Icon>
+            <Icon name="harness" size={14} className="text-tertiary-background" />
+          </NodeGroup.Icon>
+          <NodeGroup.Title>
+            <Text color="tertiaryBackground">Commits on Jun 27, 2024</Text>
+          </NodeGroup.Title>
+          <NodeGroup.Content>
+            <StackedList.Root>
+              {repos.map((repo, repo_idx) => (
+                <LinkComponent to={repo.name}>
+                  <StackedList.Item key={repo.name} isLast={repos.length - 1 === repo_idx}>
+                    <StackedList.Field description={repo.description} title={<Title title={repo.name} />} />
+                    <StackedList.Field
+                      title={
+                        <>
+                          Updated <em>{repo.timestamp}</em>
+                        </>
+                      }
+                      right
+                      label
+                      secondary
+                    />
+                  </StackedList.Item>
+                </LinkComponent>
+              ))}
+            </StackedList.Root>
+          </NodeGroup.Content>
+          <NodeGroup.Connector />
+        </NodeGroup.Root>
+      )}
+      {repos && repos.length > 0 && (
+        <NodeGroup.Root>
+          <NodeGroup.Icon>
+            <Icon name="harness" size={14} className="text-tertiary-background" />
+          </NodeGroup.Icon>
+          <NodeGroup.Title>
+            <Text color="tertiaryBackground">Commits on Jun 27, 2024</Text>
+          </NodeGroup.Title>
+          <NodeGroup.Content>
+            <StackedList.Root>
+              {repos.map((repo, repo_idx) => (
+                <LinkComponent to={repo.name}>
+                  <StackedList.Item key={repo.name} isLast={repos.length - 1 === repo_idx}>
+                    <StackedList.Field description={repo.description} title={<Title title={repo.name} />} />
+                    <StackedList.Field
+                      title={
+                        <>
+                          Updated <em>{repo.timestamp}</em>
+                        </>
+                      }
+                      right
+                      label
+                      secondary
+                    />
+                  </StackedList.Item>
+                </LinkComponent>
+              ))}
+            </StackedList.Root>
+          </NodeGroup.Content>
+          <NodeGroup.Connector />
+        </NodeGroup.Root>
+      )}
+      {repos && repos.length > 0 && (
+        <NodeGroup.Root>
+          <NodeGroup.Icon>
+            <Icon name="harness" size={14} className="text-tertiary-background" />
+          </NodeGroup.Icon>
+          <NodeGroup.Title>
+            <Text color="tertiaryBackground">Commits on Jun 27, 2024</Text>
+          </NodeGroup.Title>
+          <NodeGroup.Content>
+            <StackedList.Root>
+              {repos.map((repo, repo_idx) => (
+                <LinkComponent to={repo.name}>
+                  <StackedList.Item key={repo.name} isLast={repos.length - 1 === repo_idx}>
+                    <StackedList.Field description={repo.description} title={<Title title={repo.name} />} />
+                    <StackedList.Field
+                      title={
+                        <>
+                          Updated <em>{repo.timestamp}</em>
+                        </>
+                      }
+                      right
+                      label
+                      secondary
+                    />
+                  </StackedList.Item>
+                </LinkComponent>
+              ))}
+            </StackedList.Root>
+          </NodeGroup.Content>
+          <NodeGroup.Connector />
+        </NodeGroup.Root>
       )}
     </>
   )
