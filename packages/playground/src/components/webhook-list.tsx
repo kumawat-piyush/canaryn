327859b5fbea66f94caa0b7a2add24433d2c0070
joetaylorland <accounts+github@joetaylor.net>
2024-09-16 15:02:51 +0100

    hooked up title badges

--- packages/playground/src/components/webhook-list.tsx
+++ packages/playground/src/components/webhook-list.tsx
@@ -1,24 +1,26 @@
-import { StackedList, Text } from '@harnessio/canary'
+import {
+  Badge,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  Icon,
+  StackedList,
+  Text
+} from '@harnessio/canary'
 import React from 'react'
 
-export enum MeterState {
-  Empty = 0,
-  Error = 1,
-  Warning = 2,
-  Success = 3
+export enum WebhookState {
+  DISABLED = 'Disabled',
+  ENABLED = 'Enabled'
 }
 
 interface Webhook {
-  id: string
+  id: number
+  status: WebhookState
   name: string
-  sha?: string
   description?: string
-  version?: string
   timestamp: string
-  meter?: {
-    id: string
-    state: MeterState
-  }[]
 }
 
 interface PageProps {
@@ -26,11 +28,41 @@ interface PageProps {
   LinkComponent: React.ComponentType<{ to: string; children: React.ReactNode }>
 }
 
-const Title = ({ title }: { title: string }) => {
+const Title = ({ title, status }: { title: string; status: WebhookState }) => {
   return (
-    <div className="flex gap-2 items-center">
+    <div className="inline-flex gap-2 items-center">
       <Text truncate>{title}</Text>
+      {status && status == WebhookState.ENABLED ? (
+        // TODO: Update Badge component with standarized color variants
+        <Badge
+          variant="outline"
+          size="xs"
+          className="rounded-full border-emerald-400/20 text-emerald-300 bg-emerald-400/10">
+          <Text size={0} className="text-inherit">
+            {WebhookState.ENABLED}
+          </Text>
+        </Badge>
+      ) : (
+        <Badge variant="outline" size="xs" className="rounded-full text-tertiary-background">
+          <Text size={1} className="text-inherit">
+            {WebhookState.DISABLED}
+          </Text>
+        </Badge>
+      )}
     </div>
+  )
+}
+
+const Action = ({ id }: { id: number }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Icon name="vertical-ellipsis" size={14} className="text-tertiary-background" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="relative z-10">
+        <DropdownMenuItem key={id}>Edit webhook</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
@@ -40,10 +72,13 @@ export const WebhooksList = ({ webhooks, LinkComponent }: PageProps) => {
       {webhooks && webhooks.length > 0 && (
         <StackedList.Root>
           {webhooks.map((webhook, webhook_idx) => (
-            <LinkComponent to={webhook.id}>
+            <LinkComponent to={webhook.id.toString()}>
               <StackedList.Item key={webhook.name} isLast={webhooks.length - 1 === webhook_idx}>
-                <StackedList.Field title={<Title title="Col 1" />} />
-                <StackedList.Field label secondary title={'Col 2'} right />
+                <StackedList.Field
+                  title={<Title title={webhook.name} status={webhook.status} />}
+                  description={webhook.description}
+                />
+                <StackedList.Field label secondary title={<Action id={webhook.id} />} right />
               </StackedList.Item>
             </LinkComponent>
           ))}
