a3a673c453c85336b44fe16728671954557e8e0d
Abhinav Rastogi <abhinav.rastogi@harness.io>
2024-09-17 18:08:27 +0530

    fixes

--- apps/gitness/src/main.tsx
+++ apps/gitness/src/main.tsx
@@ -1,7 +1,4 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import '@harnessio/canary/styles'
-import '@harnessio/playground/styles'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
--- packages/playground/src/main.tsx
+++ packages/playground/src/main.tsx
@@ -2,6 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import '@harnessio/canary/styles'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
