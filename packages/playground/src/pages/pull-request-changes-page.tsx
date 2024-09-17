d23829630e0c9637506b75a1e3492fa6450629fe
joetaylorland <accounts+github@joetaylor.net>
2024-09-17 13:27:09 +0100

    add newere button variant for split

--- packages/playground/src/pages/pull-request-changes-page.tsx
+++ packages/playground/src/pages/pull-request-changes-page.tsx
@@ -2,7 +2,18 @@ import React, { useState } from 'react'
 import PlaygroundPullRequestChangesSettings from '../settings/pull-request-changes-settings'
 import { SkeletonList } from '../components/loaders/skeleton-list'
 import { NoData } from '../components/no-data'
-import { ListActions, Spacer, SplitButton } from '@harnessio/canary'
+import {
+  ListActions,
+  Spacer,
+  SplitButton,
+  Button,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  Icon,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem
+} from '@harnessio/canary'
 import * as data from '../data/mockDiffViewerdata'
 
 import PullRequestChanges from '../components/pull-request/pull-request-changes'
@@ -23,6 +34,24 @@ const FilterSortViewDropdowns: React.FC = () => {
         <SplitButton variant="outline" size="sm">
           Approve
         </SplitButton>
+        <Button
+          variant="split"
+          size="xs_split"
+          theme={'success'}
+          dropdown={
+            <DropdownMenu>
+              <DropdownMenuTrigger insideSplitButton>
+                <Icon name="chevron-down" size={11} className="chevron-down" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="mt-1">
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>Item</DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          }>
+          Approve
+        </Button>
       </ListActions.Right>
     </ListActions.Root>
   )
