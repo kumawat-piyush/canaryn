6b90c0abd6aa384d97a0d6d279bf875f053b0b19
joetaylorland <accounts+github@joetaylor.net>
2024-09-17 16:10:29 +0100

    need to finally sort layout before getting the nested menu to work, which is needed in the Files view

--- packages/playground/src/layouts/RepoLayout.tsx
+++ packages/playground/src/layouts/RepoLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { noop } from 'lodash-es'
-import { Tabs, TabsList, TabsTrigger } from '@harnessio/canary'
+import { Spacer, Tabs, TabsList, TabsTrigger } from '@harnessio/canary'
 import { NavLink, Outlet, useParams } from 'react-router-dom'
 import { TopBarWidget } from '../components/layout/top-bar-widget'
 import { mockProjects } from '../data/mockProjects'
@@ -9,37 +9,42 @@ const RepoLayout: React.FC = () => {
   const { executionId } = useParams<{ executionId: string }>()
 
   return (
-    <>
-      <TopBarWidget projects={mockProjects} onSelectProject={noop} />
-      {!executionId && (
-        <Tabs variant="navigation" defaultValue="summary">
-          <TabsList>
-            <NavLink to={`summary`}>
-              <TabsTrigger value="summary">Summary</TabsTrigger>
-            </NavLink>
-            <NavLink to={`code`}>
-              {/* Why called Files in nav but Code in figma? */}
-              <TabsTrigger value="code">Files</TabsTrigger>
-            </NavLink>
-            <NavLink to={`pipelines`}>
-              <TabsTrigger value="pipelines">Pipelines</TabsTrigger>
-            </NavLink>
-            <NavLink to={`commits`}>
-              <TabsTrigger value="commits">Commits</TabsTrigger>
-            </NavLink>
-            <NavLink to={`pull-requests`}>
-              <TabsTrigger value="pull-requests">Pull Requests</TabsTrigger>
-            </NavLink>
-            <NavLink to={`branches`}>
-              <TabsTrigger value="branches">Branches</TabsTrigger>
-            </NavLink>
-          </TabsList>
-        </Tabs>
-      )}
-      <main className="min-h-[calc(100vh-100px)] box-border overflow-hidden">
+    <div className="min-h-full">
+      <div className="sticky top-0 bg-background z-50">
+        <TopBarWidget projects={mockProjects} onSelectProject={noop} />
+        {!executionId && (
+          <Tabs variant="navigation" defaultValue="summary">
+            <TabsList>
+              <NavLink to={`summary`}>
+                <TabsTrigger value="summary">Summary</TabsTrigger>
+              </NavLink>
+              <NavLink to={`code`}>
+                {/* Why called Files in nav but Code in figma? */}
+                <TabsTrigger value="code">Files</TabsTrigger>
+              </NavLink>
+              <NavLink to={`pipelines`}>
+                <TabsTrigger value="pipelines">Pipelines</TabsTrigger>
+              </NavLink>
+              <NavLink to={`commits`}>
+                <TabsTrigger value="commits">Commits</TabsTrigger>
+              </NavLink>
+              <NavLink to={`pull-requests`}>
+                <TabsTrigger value="pull-requests">Pull Requests</TabsTrigger>
+              </NavLink>
+              <NavLink to={`branches`}>
+                <TabsTrigger value="branches">Branches</TabsTrigger>
+              </NavLink>
+            </TabsList>
+          </Tabs>
+        )}
+      </div>
+      <main className="min-h-full">
         <Outlet />
       </main>
-    </>
+      {/* TODO: avoid duplicating spacer */}
+      <Spacer size={12} />
+      <Spacer size={12} />
+    </div>
   )
 }
 
--- packages/playground/src/layouts/RootLayout.tsx
+++ packages/playground/src/layouts/RootLayout.tsx
@@ -55,7 +55,7 @@ export const RootLayout: React.FC = () => {
   ]
 
   return (
-    <div className="bg-background grid md:grid-cols-[220px_minmax(900px,_1fr)] min-w-screen">
+    <div className="bg-background grid md:grid-cols-[220px_minmax(900px,_1fr)] min-h-screen">
       {showNavbar && (
         <Navbar.Root className="max-md:hidden fixed top-0 left-0 bottom-0">
           <Navbar.Header>
@@ -89,7 +89,7 @@ export const RootLayout: React.FC = () => {
           </Navbar.Footer>
         </Navbar.Root>
       )}
-      <main className="col-start-2 min-h-screen box-border overflow-y-scroll overflow-x-hidden">
+      <main className="col-start-2">
         <Outlet />
       </main>
     </div>
