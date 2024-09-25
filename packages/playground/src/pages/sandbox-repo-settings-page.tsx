c9913ef6fe4486c21de0839b5e3f4a879b5c469d
joetaylorland <accounts+github@joetaylor.net>
2024-09-25 11:15:44 +0100

    add side navbar

--- packages/playground/src/pages/sandbox-repo-settings-page.tsx
+++ packages/playground/src/pages/sandbox-repo-settings-page.tsx
@@ -1,6 +1,38 @@
 import React, { useState } from 'react'
 import { PlaygroundListSettings } from '../settings/list-settings'
 import { SandboxLayout } from '../index'
+import { Navbar } from '@harnessio/canary'
+
+function Sidebar() {
+  return (
+    <Navbar.Root className="bg-transparent border-none">
+      <Navbar.Content>
+        <Navbar.Group title="Access">
+          <Navbar.Item text="Collaborations" />
+          <Navbar.Item text="Moderation options" />
+        </Navbar.Group>
+        <Navbar.Group title="Code and automation" topBorder>
+          <Navbar.Item text="Branches" />
+          <Navbar.Item text="Tags" />
+          <Navbar.Item text="Rules" />
+          <Navbar.Item text="Actions" />
+          <Navbar.Item text="Webhooks" />
+          <Navbar.Item text="Environments" />
+          <Navbar.Item text="Codespaces" />
+          <Navbar.Item text="Pages" />
+        </Navbar.Group>
+        <Navbar.Group title="Security" topBorder>
+          <Navbar.Item text="Code security and analysis" />
+          <Navbar.Item text="Deploy keys" />
+          <Navbar.Item text="Secrets and variables" />
+        </Navbar.Group>
+        <Navbar.Group title="Security" topBorder>
+          <Navbar.Item text="Email notifications" />
+        </Navbar.Group>
+      </Navbar.Content>
+    </Navbar.Root>
+  )
+}
 
 function SandboxRepoSettingsPage() {
   const [loadState, setLoadState] = useState('data-loaded')
@@ -9,8 +41,8 @@ function SandboxRepoSettingsPage() {
     <SandboxLayout.Main hasHeader hasSubHeader hasLeftPanel>
       <SandboxLayout.Columns columnWidths="auto 1fr">
         <SandboxLayout.Column>
-          <SandboxLayout.Content>
-            <p>Menu</p>
+          <SandboxLayout.Content className="pr-0">
+            <Sidebar />
           </SandboxLayout.Content>
         </SandboxLayout.Column>
         <SandboxLayout.Column>
