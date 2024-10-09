5ab72ff240752c9fa694c7a455599a24b7c7b71d
Vardan Bansal <vardan.bansal@harness.io>
2024-10-08 22:03:08 -0700

    cleanup

--- apps/gitness/src/framework/hooks/useGetSpaceParam.ts
+++ apps/gitness/src/framework/hooks/useGetSpaceParam.ts
@@ -1,9 +1,7 @@
 import { useParams } from 'react-router-dom'
 import type { PathParams } from '../../RouteDefinitions'
-import { useAppContext } from '../context/AppContext'
 
 export function useGetSpaceURLParam(): string | undefined {
-  const spaces = useAppContext()
   const { spaceId } = useParams<PathParams>()
-  return spaceId || spaces?.spaces?.[0]?.space?.path
+  return spaceId
 }
