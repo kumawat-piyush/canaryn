85e708d673318f910f4e069d3deb3826b67c2249
jessieyu777 <c_peipeijessie@harness.io>
2024-10-15 16:39:07 -0700

    add mock members data

---
+++ packages/playground/src/data/mockMembersData.ts
@@ -0,0 +1,106 @@
+export const mockMemberData = [
+  {
+    display_name: 'John Doe',
+    role: 'Admin',
+    email: 'johndoe@gmail.com',
+    avatarUrl: '../public/images/user-avatar.svg',
+    timestamp: '2 days ago'
+  },
+  {
+    display_name: 'Jane Smith',
+    role: 'Member',
+    email: 'janesmith@yahoo.com',
+    avatarUrl: '../public/images/user-avatar.svg',
+    timestamp: '3 days ago'
+  },
+  {
+    display_name: 'Michael Brown',
+    role: 'Editor',
+    email: 'michaelbrown@hotmail.com',
+    avatarUrl: '../public/images/user-avatar.svg',
+    timestamp: '4 hours ago'
+  },
+  {
+    display_name: 'Emily Davis',
+    role: 'Admin',
+    email: 'emilydavis@outlook.com',
+    avatarUrl: '../public/images/user-avatar.svg',
+    timestamp: '1 hour ago'
+  },
+  {
+    display_name: 'William Johnson',
+    role: 'Member',
+    email: 'williamjohnson@gmail.com',
+    avatarUrl: '../public/images/user-avatar.svg',
+    timestamp: '5 days ago'
+  },
+  {
+    display_name: 'Olivia Wilson',
+    role: 'Editor',
+    email: 'oliviawilson@live.com',
+    timestamp: '1 week ago'
+  },
+  {
+    display_name: 'James Miller',
+    role: 'Member',
+    email: 'jamesmiller@protonmail.com',
+    avatarUrl: '../public/images/user-avatar.svg',
+    timestamp: '30 minutes ago'
+  },
+  {
+    display_name: 'Sophia Moore',
+    role: 'Admin',
+    email: 'sophiamoore@gmail.com',
+    avatarUrl: '',
+    timestamp: '3 hours ago'
+  },
+  {
+    display_name: 'Liam Anderson',
+    role: 'Member',
+    email: 'liamanderson@yahoo.com',
+    avatarUrl: '../public/images/user-avatar.svg',
+    timestamp: '2 days ago'
+  },
+  {
+    display_name: 'Mia Thomas',
+    role: 'Editor',
+    email: 'miathomas@outlook.com',
+    avatarUrl: '',
+    timestamp: '10 minutes ago'
+  },
+  {
+    display_name: 'Lucas Martin',
+    role: 'Member',
+    email: 'lucasmartin@hotmail.com',
+    avatarUrl: '',
+    timestamp: '2 weeks ago'
+  },
+  {
+    display_name: 'Ava Jackson',
+    role: 'Admin',
+    email: 'avajackson@protonmail.com',
+    avatarUrl: '',
+    timestamp: '15 minutes ago'
+  },
+  {
+    display_name: 'Ethan White',
+    role: 'Editor',
+    email: 'ethanwhite@live.com',
+    avatarUrl: '',
+    timestamp: '5 days ago'
+  },
+  {
+    display_name: 'Isabella Harris',
+    role: 'Member',
+    email: 'isabellaharris@gmail.com',
+    avatarUrl: '',
+    timestamp: '20 minutes ago'
+  },
+  {
+    display_name: 'Noah Clark',
+    role: 'Admin',
+    email: 'noahclark@yahoo.com',
+    avatarUrl: '',
+    timestamp: '1 day ago'
+  }
+]
