675af74ffeed6add90ce3cdc991d55bd7b479a1a
sanskar <c_sanskar.sehgal@harness.io>
2024-10-16 19:16:37 -0700

    eslint :(

--- packages/playground/src/components/repo-settings/repo-settings-general/repo-settings-general-security.tsx
+++ packages/playground/src/components/repo-settings/repo-settings-general/repo-settings-general-security.tsx
@@ -16,54 +16,54 @@ export type FormFields = z.infer<typeof formSchema> // Automatically generate a 
 //   onFormSubmit?: (data: FormFields) => void
 //   apiError: string | null
 // }
-export const RepoSettingsSecurityForm: React.FC /*<RepoSettingsSecurityFormProps>*/ = (
+export const RepoSettingsSecurityForm: React.FC /*<RepoSettingsSecurityFormProps>*/ = () =>
+  // {
+  // onFormSubmit apiError = null
+  // }
   {
-    // onFormSubmit apiError = null
-  }
-) => {
-  const {
-    handleSubmit,
-    setValue,
-    watch,
-    formState: { errors }
-  } = useForm<FormFields>({
-    resolver: zodResolver(formSchema),
-    mode: 'onChange',
-    defaultValues: {
-      secretScanning: false
+    const {
+      handleSubmit,
+      setValue,
+      watch,
+      formState: { errors }
+    } = useForm<FormFields>({
+      resolver: zodResolver(formSchema),
+      mode: 'onChange',
+      defaultValues: {
+        secretScanning: false
+      }
+    })
+    const onCheckboxChange = (checked: boolean) => {
+      setValue('secretScanning', checked)
+      handleSubmit(data => {
+        console.log(data)
+        // onFormSubmit(data)
+      })()
     }
-  })
-  const onCheckboxChange = (checked: boolean) => {
-    setValue('secretScanning', checked)
-    handleSubmit(data => {
-      console.log(data)
-      // onFormSubmit(data)
-    })()
-  }
-  return (
-    <>
-      <Text size={4} weight="medium">
-        Security
-      </Text>
-      <FormFieldSet.Root className="mb-0">
-        <FormFieldSet.ControlGroup>
-          <FormFieldSet.Option
-            className="mt-0"
-            control={
-              <Checkbox checked={watch('secretScanning')} id="secret-scanning" onCheckedChange={onCheckboxChange} />
-            }
-            id="secret-scanning"
-            label="Secret scanning"
-            description="Block commits containing secrets like passwords, API keys and tokens."
-          />
-          {errors.secretScanning && (
-            <FormFieldSet.Message theme={FormFieldSet.MessageTheme.ERROR}>
-              {errors.secretScanning.message?.toString()}
-            </FormFieldSet.Message>
-          )}
-        </FormFieldSet.ControlGroup>
+    return (
+      <>
+        <Text size={4} weight="medium">
+          Security
+        </Text>
+        <FormFieldSet.Root className="mb-0">
+          <FormFieldSet.ControlGroup>
+            <FormFieldSet.Option
+              className="mt-0"
+              control={
+                <Checkbox checked={watch('secretScanning')} id="secret-scanning" onCheckedChange={onCheckboxChange} />
+              }
+              id="secret-scanning"
+              label="Secret scanning"
+              description="Block commits containing secrets like passwords, API keys and tokens."
+            />
+            {errors.secretScanning && (
+              <FormFieldSet.Message theme={FormFieldSet.MessageTheme.ERROR}>
+                {errors.secretScanning.message?.toString()}
+              </FormFieldSet.Message>
+            )}
+          </FormFieldSet.ControlGroup>
 
-        {/* {apiError && (
+          {/* {apiError && (
           <>
             <Spacer size={2} />
             <Text size={1} className="text-destructive">
@@ -71,7 +71,7 @@ export const RepoSettingsSecurityForm: React.FC /*<RepoSettingsSecurityFormProps
             </Text>
           </>
         )} */}
-      </FormFieldSet.Root>
-    </>
-  )
-}
+        </FormFieldSet.Root>
+      </>
+    )
+  }
