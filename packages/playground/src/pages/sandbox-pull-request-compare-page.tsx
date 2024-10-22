e4e830e26ca570084cfda571a50f1319c14da8ab
Calvin Lee <cjlee@ualberta.ca>
2024-10-21 19:11:42 -0600

    feat: [pipe-22241]: update compare pr page- pt 3
    Signed-off-by: Calvin Lee <cjlee@ualberta.ca>

--- packages/playground/src/pages/sandbox-pull-request-compare-page.tsx
+++ packages/playground/src/pages/sandbox-pull-request-compare-page.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import { noop } from 'lodash-es'
 import { SandboxPullRequestCompare } from '../layouts/SandboxPullRequestCompareLayout'
 import { mockCommitData } from '../data/mockCommitData'
-
-const mockBranchList = [{ name: 'main' }, { name: 'new-feature' }, { name: 'test-wip' }, { name: 'display-db' }]
+import { mockBranchList } from '../data/mockBranchList'
 
 const SandboxPullRequestComparePage = () => {
   return (
