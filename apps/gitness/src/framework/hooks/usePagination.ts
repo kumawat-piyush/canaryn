d6d7ed4022a79aa7bf30dddbc4a17409d7199489
Vardan Bansal <vardan.bansal@harness.io>
2024-10-28 08:50:02 -0700

    revert changes to use pagination hook

--- apps/gitness/src/framework/hooks/usePagination.ts
+++ apps/gitness/src/framework/hooks/usePagination.ts
@@ -1,27 +1,28 @@
 import { useState } from 'react'
-import usePageResponseHeaders from '../../utils/page-utils'
 
-interface Pagination {
-  totalPages: number
-  currentPage: number
-  previousPage: () => void
-  nextPage: () => void
-  handleClick: (page: number) => void
-}
-
-export const usePagination = (responseHeaders: HeadersInit, initialPage: number = 1): Pagination => {
-  const { xTotalPages: totalPages = 1 } = usePageResponseHeaders(responseHeaders)
+export const usePagination = (initialPage: number = 1, totalPages: number = 1) => {
   const [currentPage, setCurrentPage] = useState<number>(initialPage)
 
-  const handleClick = (page: number): void => setCurrentPage(page)
-
-  const previousPage = (): void => {
-    if (currentPage > 1) handleClick(currentPage - 1)
+  const handleClick = (page: number) => {
+    setCurrentPage(page)
   }
 
-  const nextPage = (): void => {
-    if (currentPage < totalPages) handleClick(currentPage + 1)
+  const previousPage = () => {
+    if (currentPage > 1) {
+      handleClick(currentPage - 1)
+    }
   }
 
-  return { totalPages, currentPage, previousPage, nextPage, handleClick }
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      handleClick(currentPage + 1)
+    }
+  }
+
+  return {
+    currentPage,
+    previousPage,
+    nextPage,
+    handleClick
+  }
 }
