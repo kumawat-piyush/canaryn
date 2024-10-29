d11cdcb954550d4c9b390f747423eafe9edf3cd1
Vardan Bansal <vardan.bansal@harness.io>
2024-10-29 01:25:29 -0700

    migrate sandbox file explorer

--- apps/gitness/src/components/SandboxFileViewer.tsx
+++ apps/gitness/src/components/SandboxFileViewer.tsx
@@ -15,7 +15,6 @@ import { SkeletonList, Summary, FileProps, SummaryItemType, NoData, SandboxLayou
 import {
   useGetContentQuery,
   pathDetails,
-  RepoPathsDetailsOutput,
   GitPathDetails,
   OpenapiGetContentOutput,
   OpenapiContentInfo
@@ -38,11 +37,13 @@ export const SandboxFileViewer: React.FC = () => {
   const [loading, setLoading] = useState(false)
   console.log(subResourcePath)
 
-  const { data: repoDetails } = useGetContentQuery({
+  const { data } = useGetContentQuery({
     path: fullResourcePath || '',
     repo_ref: repoRef,
     queryParams: { include_commit: true, git_ref: normalizeGitRef(gitRef || '') }
   })
+
+  const repoDetails = data?.body
 
   const repoEntryPathToFileTypeMap = useMemo((): Map<string, OpenapiGetContentOutput['type']> => {
     if (repoDetails?.content?.entries?.length === 0) return new Map()
@@ -74,10 +75,10 @@ export const SandboxFileViewer: React.FC = () => {
         body: { paths: Array.from(repoEntryPathToFileTypeMap.keys()) },
         repo_ref: repoRef
       })
-        .then((response: RepoPathsDetailsOutput) => {
-          if (response?.details && response.details.length > 0) {
+        .then(response => {
+          if (response?.body?.details && response.body.details.length > 0) {
             setFiles(
-              response.details.map(
+              response.body.details.map(
                 (item: GitPathDetails) =>
                   ({
                     id: item?.path || '',
