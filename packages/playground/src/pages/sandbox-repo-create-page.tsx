c84ce6452380863b7f150ebbf46b5b6e2db3d4e9
sanskar <c_sanskar.sehgal@harness.io>
2024-11-07 22:54:19 -0800

    feat: add gitignore and license values

--- packages/playground/src/pages/sandbox-repo-create-page.tsx
+++ packages/playground/src/pages/sandbox-repo-create-page.tsx
@@ -29,8 +29,8 @@ import { MessageTheme } from '../components/form-field-set'
 const formSchema = z.object({
   name: z.string().min(1, { message: 'Please provide a name' }),
   description: z.string(),
-  gitignore: z.enum(['', '1', '2', '3']),
-  license: z.enum(['', '1', '2', '3']),
+  gitignore: z.string().optional(),
+  license: z.string().optional(),
   access: z.enum(['1', '2'], { errorMap: () => ({ message: 'Please select who has access' }) })
 })
 
@@ -42,13 +42,17 @@ interface SandboxRepoCreatePageProps {
   apiError: string | null
   isLoading: boolean
   isSuccess: boolean
+  gitIgnoreOptions?: string[]
+  licenseOptions?: { value?: string; label?: string }[]
 }
 const SandboxRepoCreatePage: React.FC<SandboxRepoCreatePageProps> = ({
   onFormSubmit,
   apiError = null,
   onFormCancel,
   isLoading,
-  isSuccess
+  isSuccess,
+  gitIgnoreOptions,
+  licenseOptions
 }) => {
   const {
     register,
@@ -65,7 +69,7 @@ const SandboxRepoCreatePage: React.FC<SandboxRepoCreatePageProps> = ({
       description: '',
       gitignore: '',
       license: '',
-      access: '1'
+      access: '2'
     }
   })
 
@@ -145,9 +149,7 @@ const SandboxRepoCreatePage: React.FC<SandboxRepoCreatePageProps> = ({
             {/* ACCESS */}
             <FormFieldSet.Root box shaded>
               <FormFieldSet.ControlGroup>
-                <FormFieldSet.Label htmlFor="access" required>
-                  Who has access?
-                </FormFieldSet.Label>
+                <FormFieldSet.Label htmlFor="access">Who has access?</FormFieldSet.Label>
                 <RadioGroup value={accessValue} onValueChange={handleAccessChange} id="access">
                   <FormFieldSet.Option
                     control={<RadioGroupItem value="1" id="access-public" />}
@@ -179,9 +181,14 @@ const SandboxRepoCreatePage: React.FC<SandboxRepoCreatePageProps> = ({
                     <SelectValue placeholder="Select" />
                   </SelectTrigger>
                   <SelectContent>
-                    <SelectItem value="1">.gitignore option 1</SelectItem>
-                    <SelectItem value="2">.gitignore option 2</SelectItem>
-                    <SelectItem value="3">.gitignore option 3</SelectItem>
+                    {gitIgnoreOptions &&
+                      gitIgnoreOptions?.map(option => {
+                        return (
+                          <SelectItem key={option} value={option}>
+                            {option}
+                          </SelectItem>
+                        )
+                      })}
                   </SelectContent>
                 </Select>
                 {errors.gitignore && (
@@ -199,9 +206,12 @@ const SandboxRepoCreatePage: React.FC<SandboxRepoCreatePageProps> = ({
                     <SelectValue placeholder="Select" />
                   </SelectTrigger>
                   <SelectContent>
-                    <SelectItem value="1">License option 1</SelectItem>
-                    <SelectItem value="2">License option 2</SelectItem>
-                    <SelectItem value="3">License option 3</SelectItem>
+                    {licenseOptions &&
+                      licenseOptions?.map(option => (
+                        <SelectItem key={option.value} value={option.value ?? ''}>
+                          {option.label}
+                        </SelectItem>
+                      ))}
                   </SelectContent>
                 </Select>
                 <FormFieldSet.Caption>
