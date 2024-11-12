91c29739e3eeec608246feaa720d1db1d587308d
Abhinav Rastogi <abhinav.rastogi@harness.io>
2024-11-11 16:42:17 -0800

    fix repo settings again

--- packages/playground/src/App.tsx
+++ packages/playground/src/App.tsx
@@ -57,7 +57,7 @@ import { SandboxSettingsProjectPage } from './pages/sandbox-settings-project-pag
 import { SandboxSettingsProjectGeneralPage } from './pages/sandbox-settings-project-general-page'
 import { SandboxSettingsProjectMembersPage } from './pages/sandbox-settings-project-members-page'
 import { SandboxRepoCreatePage } from './pages/sandbox-repo-create-page'
-import { SandboxRepoSettingsPage } from './pages/sandbox-repo-settings-page'
+import { RepoSettingsPage } from './components/repo-settings-page'
 import { RepoSettingsGeneralPlaygroundContainer } from './pages/repo-settings-general-page-playground-container'
 import { RepoSettingsCollaborationsPage } from './pages/repo-settings-collaborations-page'
 import { RepoSettingsModerationPage } from './pages/repo-settings-moderation-page'
@@ -214,7 +214,7 @@ const router = createBrowserRouter([
               },
               {
                 path: 'settings',
-                element: <SandboxRepoSettingsPage />,
+                element: <RepoSettingsPage />,
                 children: [
                   {
                     index: true,
