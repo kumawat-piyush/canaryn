bb115801c45647acebea847f0c06c8e8246e6b47
Pranesh <pranesh.g@harness.io>
2024-11-15 13:47:16 +0530

    fix: [PIPE-23302]: Fix Eslint issues in unified pipeline

--- packages/unified-pipeline/src/components/Canvas/Canvas.tsx
+++ packages/unified-pipeline/src/components/Canvas/Canvas.tsx
@@ -142,7 +142,7 @@ const CanvasInternal = (props: CanvasProps) => {
   )
 
   return (
-    <div className="w-full h-full canvas-container" onMouseMove={handleMouseMove}>
+    <div className="canvas-container size-full" onMouseMove={handleMouseMove}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
