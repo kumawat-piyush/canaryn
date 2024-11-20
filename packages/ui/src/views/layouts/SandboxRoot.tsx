707ec2adf9b652761aa3eda7a4d19a5ef5b5ba06
sanskar <c_sanskar.sehgal@harness.io>
2024-11-20 15:17:26 -0800

    feat: start adding enw UI
    fix: tooling
    
    fix: tooling, feat: bring over a few components
    
    fix: fix playground
    
    feat: adding components from PP to UI
    
    feat: move more components
    
    feat: move components/clean-up
    
    feat: adding components, get something working
    
    feat: adding components, trying to fix tsconfig
    
    feat: adding components from PP
    
    feat: tailwind working
    
    add new harness-oss app
    
    remove canary references in ui package
    
    fix lint issues
    
    run prettier
    
    remove harness-oss

---
+++ packages/ui/src/views/layouts/SandboxRoot.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { SandboxLayout } from '../index'
+import { Outlet } from 'react-router-dom'
+import { Navbar } from '../navbar'
+import { MoreSubmenu } from '../../components/more-submenu'
+import type { TypesUser } from './types'
+import { SettingsMenu } from '../../components/settings-menu'
+
+interface SandboxRootProps {
+  currentUser: TypesUser | undefined
+}
+
+export const SandboxRoot: React.FC<SandboxRootProps> = ({ currentUser }) => {
+  const [showMore, setShowMore] = useState(false)
+  const [showSystemAdmin, setShowSystemAdmin] = useState(false)
+
+  const handleMore = () => {
+    setShowSystemAdmin(false)
+    setShowMore(prevState => !prevState)
+  }
+
+  const handleSystemAdmin = () => {
+    setShowMore(false)
+    setShowSystemAdmin(prevState => !prevState)
+  }
+
+  return (
+    <SandboxLayout.Root>
+      <SandboxLayout.LeftPanel>
+        <Navbar
+          showMore={showMore}
+          showSystemAdmin={showSystemAdmin}
+          handleMore={handleMore}
+          handleSystemAdmin={handleSystemAdmin}
+          currentUser={currentUser}
+        />
+      </SandboxLayout.LeftPanel>
+      <Outlet />
+      <MoreSubmenu showMore={showMore} handleMore={handleMore} />
+      <SettingsMenu showSystemAdmin={showSystemAdmin} handleSystemAdmin={handleSystemAdmin} />
+    </SandboxLayout.Root>
+  )
+}
