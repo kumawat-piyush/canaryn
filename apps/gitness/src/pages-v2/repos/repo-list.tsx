f39bf0d3a35bcd821184f7b24e3ece57c02ec661
sanskar <c_sanskar.sehgal@harness.io>
2024-11-21 17:33:48 -0800

    fix: comments, props, types etc

--- apps/gitness/src/pages-v2/repos/repo-list.tsx
+++ apps/gitness/src/pages-v2/repos/repo-list.tsx
@@ -15,7 +15,6 @@ export default function ReposListPage() {
   const space = useGetSpaceURLParam() ?? ''
 
   /* Query and Pagination */
-  // const { sort } = useCommonFilter<ListReposQueryQueryParams['sort']>()
   const [query] = useDebouncedQueryState('query')
   const [page, setPage] = useQueryState('page', parseAsInteger.withDefault(1))
   const [repos, setRepos] = useState<RepositoryType[] | null>(null)
@@ -71,73 +70,6 @@ export default function ReposListPage() {
   })
 
   const totalPages = parseInt(headers?.get(PageResponseHeader.xTotalPages) || '')
-
-  //   const renderListContent = () => {
-  // if (isFetching) return <SkeletonList />
-
-  // if (isError)
-  //   return (
-  //     <NoData
-  //       title="Error"
-  //       description={[error.message || '']}
-  //       primaryButton={{
-  //         label: 'Go Back',
-  //         onClick: () => {
-  //           navigate(-1)
-  //         }
-  //       }}
-  //     />
-  //   )
-
-  // if (!repositories?.length) {
-  //   if (query) {
-  //     return (
-  //       <NoSearchResults
-  //         iconName="no-search-magnifying-glass"
-  //         title="No search results"
-  //         description={['Check your spelling and filter options,', 'or search for a different keyword.']}
-  //         primaryButton={{ label: 'Clear search', onClick: () => setQuery('') }}
-  //       />
-  //     )
-  //   }
-  //   return (
-  //     <NoData
-  //       iconName="no-data-folder"
-  //       title="No repositories yet"
-  //       description={[
-  //         'There are no repositories in this project yet.',
-  //         'Create new or import an existing repository.'
-  //       ]}
-  //       primaryButton={{ label: 'Create repository', to: `create` }}
-  //       /**
-  //        * @TODO add "to" link when Import repository gets implemented
-  //        */
-  //       secondaryButton={{ label: 'Import repository', to: 'import' }}
-  //     />
-  //   )
-  // }
-
-  //     return (
-  //       <RepoList
-  //         LinkComponent={LinkComponent}
-  //         repos={repositories?.map((repo: RepoRepositoryOutput) => {
-  //           return {
-  //             id: repo.id || 0,
-  //             name: repo.identifier,
-  //             description: repo.description,
-  //             private: !repo.is_public,
-  //             stars: 0,
-  //             forks: repo.num_forks,
-  //             pulls: repo.num_pulls,
-  //             timestamp: (repo.updated && timeAgoFromEpochTime(repo.updated)) || '',
-  //             importing: repo.importing
-  //           }
-  //         })}
-  //       />
-  //     )
-  //   }
-
-  //   const repositoriesExist = (repositories?.length || 0) > 0
 
   return (
     <SandboxRepoListPage
